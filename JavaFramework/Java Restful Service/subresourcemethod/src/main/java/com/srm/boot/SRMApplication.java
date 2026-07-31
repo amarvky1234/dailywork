04ed7a6fe6195fc7ee9b0679bf334efac1305083
Amarjeet Kumar <amarvky1234@gmail.com>
2026-07-31 11:18:13 +0530

    restful subresourcemethod example

---
+++ JavaFramework/Java Restful Service/subresourcemethod/src/main/java/com/srm/boot/SRMApplication.java
@@ -0,0 +1,9 @@
+package com.srm.boot;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class SRMApplication extends Application {
+
+}
