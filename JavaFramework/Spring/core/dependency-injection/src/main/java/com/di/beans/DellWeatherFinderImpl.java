b751ac86e2d652b3076ee0bfbe107ed2d3c1979d
Amarjeet Kumar <amarvky1234@gmail.com>
2026-08-06 19:09:04 +0530

    core dependency-injection example

---
+++ JavaFramework/Spring/core/dependency-injection/src/main/java/com/di/beans/DellWeatherFinderImpl.java
@@ -0,0 +1,9 @@
+package com.di.beans;
+
+public class DellWeatherFinderImpl implements IWeatherFinder{
+    @Override
+    public double getWeather(String zipcode) {
+        return 27;
+    }
+}
+
---
+++ JavaFramework/Spring/core/dependency-injection/src/main/java/com/di/beans/OracleWeatherFinderImpl.java
@@ -0,0 +1,8 @@
+package com.di.beans;
+
+public class OracleWeatherFinderImpl implements IWeatherFinder{
+    @Override
+    public double getWeather(String zipcode) {
+        return 45;
+    }
+}
